7cff60a72f25ee3d2a3b8ba9ccc65418682fc5d1
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-09-27 13:32:48 +0700

    Update language.py

--- HasiiMusic/utils/decorators/language.py
+++ HasiiMusic/utils/decorators/language.py
@@ -10,7 +10,7 @@ def language(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    text=f"{app.mention} Đang được bảo trì, hãy truy cập <a href={SUPPORT_CHAT}>nhóm hỗ trợ</a> để biết lý do.",
                     disable_web_page_preview=True,
                 )
         try:
@@ -33,7 +33,7 @@ def languageCB(mystic):
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    f"{app.mention} Đang được bảo trì, hãy truy cập hỗ trợ chat để biết lý do.",
                     show_alert=True,
                 )
         try:
