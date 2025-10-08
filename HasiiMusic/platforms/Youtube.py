acfc7794773045d84ba625f91b39862cc39807f4
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-10-09 03:30:35 +0700

    Update Youtube.py

--- HasiiMusic/platforms/Youtube.py
+++ HasiiMusic/platforms/Youtube.py
@@ -204,7 +204,7 @@ class YouTubeAPI:
             link = self.playlist_url + str(videoid)
         if "&" in link:
             link = link.split("&")[0]
-        stdout, _ = await _exec_proc(
+        stdout = await _exec_proc(
             "yt-dlp",
             *(_cookies_args()),
             "-i",
